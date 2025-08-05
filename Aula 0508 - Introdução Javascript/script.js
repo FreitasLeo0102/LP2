276ca0ddbda7d2cf89253f0eec5e5bbbd9d57553
FreitasLeo0102 <freitasleo535@gmail.com>
2025-08-05 17:33:20 -0300

    Add files via upload

---
+++ Aula 0508 - Introdução Javascript/script.js
@@ -0,0 +1,13 @@
+alert('Ola Mundo')
+
+const a = 9 // variavel "const" não pode ser alterada, variavel local
+let b = 10 // variavel "let" pode ser alterada, variavel local
+var c = 10 // variavel "var" pode ser alterada, variavel global
+
+console.log("O valor de a é: " + a) // o "+" é usado para concatenação
+
+
+
+console.log('a + b') // as '' atribuem valor de texto
+console.log(a + b) // sem as '' a operção é feita com oas numeros
+
